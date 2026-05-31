9cddd27bab0a91957ec5ce1fe9b99fd66d71cc06
ayan-ahmed-studio <ayanahmed3538@gmail.com>
2026-05-31 18:45:58 +0500

    Update SignatureDishesSection.jsx

--- src/components/SignatureDishesSection.jsx
+++ src/components/SignatureDishesSection.jsx
@@ -1,1 +1,10 @@
 
+function SignatureDishesSection() {
+ import DishCard from "./DishCard";
+import useFadeIn from "../hooks/useFadeIn";
+import { PROJECTS, gradText } from "../data/data";
+
+export default function SignatureDishesSection() {
+   // paste code
+}
+}
