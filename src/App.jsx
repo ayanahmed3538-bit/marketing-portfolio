f0ea1ebf4cb1d894278babdf98183ba0f1e02daf
ayan-ahmed-studio <ayanahmed3538@gmail.com>
2026-05-31 18:47:13 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,26 @@
 
+import "./styles/global.css";
+
+import HeroSection from "./components/HeroSection";
+import MarqueeSection from "./components/MarqueeSection";
+import AboutSection from "./components/AboutSection";
+import MenuSection from "./components/MenuSection";
+import SignatureDishesSection from "./components/SignatureDishesSection";
+
+export default function App() {
+  return (
+    <div
+      style={{
+        background: "#0C0C0C",
+        fontFamily: "'Kanit','Inter',sans-serif",
+        overflowX: "clip",
+      }}
+    >
+      <HeroSection />
+      <MarqueeSection />
+      <AboutSection />
+      <MenuSection />
+      <SignatureDishesSection />
+    </div>
+  );
+}
