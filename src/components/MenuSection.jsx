97c5ca6364f187c8fe3764bb7f11f4225ecd49d5
ayan-ahmed-studio <ayanahmed3538@gmail.com>
2026-05-31 18:44:42 +0500

    Update MenuSection.jsx

--- src/components/MenuSection.jsx
+++ src/components/MenuSection.jsx
@@ -1,1 +1,9 @@
 
+function MenuSection() {
+ import useFadeIn from "../hooks/useFadeIn";
+import { MENU_ITEMS } from "../data/data";
+
+export default function MenuSection() {
+   // paste code
+}
+}
