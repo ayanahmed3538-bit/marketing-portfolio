605afe6c396518912a92132f2b86b0ca1996d510
ayan-ahmed-studio <ayanahmed3538@gmail.com>
2026-05-31 18:39:19 +0500

    Update data.js

--- src/data/data.js
+++ src/data/data.js
@@ -1,1 +1,18 @@
 
+export const FOOD_IMGS = [ ... ];
+export const MENU_ITEMS = [ ... ];
+export const PROJECTS = [ ... ];
+
+export const gradText = {
+  background: "linear-gradient(180deg,#8a6a2a 0%,#D4A847 50%,#F5D78E 100%)",
+  WebkitBackgroundClip: "text",
+  WebkitTextFillColor: "transparent",
+  backgroundClip: "text",
+};
+
+export const gradTextLight = {
+  background: "linear-gradient(180deg,#646973 0%,#BBCCD7 100%)",
+  WebkitBackgroundClip: "text",
+  WebkitTextFillColor: "transparent",
+  backgroundClip: "text",
+};
