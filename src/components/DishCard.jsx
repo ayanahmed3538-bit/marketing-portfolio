c651677185a9379d67dd4581b65c040337418298
ayan-ahmed-studio <ayanahmed3538@gmail.com>
2026-05-31 18:45:22 +0500

    Update DishCard.jsx

--- src/components/DishCard.jsx
+++ src/components/DishCard.jsx
@@ -1,1 +1,9 @@
 
+function DishCard({ p, index }) {
+ import { useEffect, useRef, useState } from "react";
+import { gradText } from "../data/data";
+
+export default function DishCard({ p, index }) {
+   // paste code
+}
+}
