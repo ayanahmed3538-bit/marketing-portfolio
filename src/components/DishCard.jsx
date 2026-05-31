660aeb083d40be215905507b95b0980892b6af7b
ayan-ahmed-studio <ayanahmed3538@gmail.com>
2026-05-31 20:36:24 +0500

    Delete src directory

--- src/components/DishCard.jsx
+++
@@ -1,9 +0,0 @@
-
-function DishCard({ p, index }) {
- import { useEffect, useRef, useState } from "react";
-import { gradText } from "../data/data";
-
-export default function DishCard({ p, index }) {
-   // paste code
-}
-}
--- src/components/MarqueeSection.jsx
+++
@@ -1,9 +0,0 @@
-
-function MarqueeSection() {
- import { useEffect, useRef, useState } from "react";
-import { FOOD_IMGS } from "../data/data";
-
-export default function MarqueeSection() {
-   // paste code
-}
-}
