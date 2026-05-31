b8e2ee9d8d0aec7073ffe2ded5bfb5322c2e5e4d
ayan-ahmed-studio <ayanahmed3538@gmail.com>
2026-05-31 18:43:34 +0500

    Update MarqueeSection.jsx

--- src/components/MarqueeSection.jsx
+++ src/components/MarqueeSection.jsx
@@ -1,1 +1,9 @@
 
+function MarqueeSection() {
+ import { useEffect, useRef, useState } from "react";
+import { FOOD_IMGS } from "../data/data";
+
+export default function MarqueeSection() {
+   // paste code
+}
+}
