430d2866a98421d40d1a7a44a8b3148fdbf27c10
ayan-ahmed-studio <ayanahmed3538@gmail.com>
2026-05-31 18:42:32 +0500

    Update HeroSection.jsx

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,4 +1,10 @@
 
 function HeroSection() {
- ...
+import { useEffect, useState } from "react";
+import BookButton from "./BookButton";
+import { gradText } from "../data/data";
+
+export default function HeroSection() {
+   // paste code
+}
 }
